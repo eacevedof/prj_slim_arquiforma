5f1da90778012571372a3abbc2aeb0277fece106
Eduardo A.F. <eacevedof@yahoo.es>
2025-03-08 13:04:14 +0100

    seo

--- backend_web/src/Modules/Open/Home/Application/GetHomePage/GetHomePageService.php
+++ backend_web/src/Modules/Open/Home/Application/GetHomePage/GetHomePageService.php
@@ -9,12 +9,10 @@ final readonly class GetHomePageService
     public function __invoke(): array
     {
         return [
-            "message" => "
-ARQUIFORMA Y OBRAS EN GENERAL SL
-B-56883473
-C/ GENERAL RICARDOS 105
-28019 Madrid
-"
+            "ARQUIFORMA Y OBRAS EN GENERAL SL
+            B-56883473
+            C/ GENERAL RICARDOS 105
+            28019 Madrid"
         ];
     }
 }
