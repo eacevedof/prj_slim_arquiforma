b31c25d84623c8fc5b9b8ecefa1c16d81cc803d6
Eduardo A.F. <eacevedof@yahoo.es>
2025-03-08 12:24:01 +0100

    welcome ok

--- backend_web/app/routes.php
+++ backend_web/app/routes.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
