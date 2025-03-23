80eb987c0f625732661a9098df2229bc0b2aaafb
Eduardo A.F. <eacevedof@yahoo.es>
2025-03-23 21:58:54 +0100

    xml reader ok

--- backend_web/public/yog/Enums/XmlTagEnum.php
+++ backend_web/public/yog/Enums/XmlTagEnum.php
@@ -21,6 +21,16 @@ enum XmlTagEnum: string
     case FIELD_INFORMATION = "f_i";
     case SERVER_INFO = "s_v";
     case M_I = "m_i";
+    case E_I = "e_i";
     case NUM_ROW_RESULT = "a_r";
+
+    case FIELD_NAME = "n";
+    case FIELD_TABLE = "t";
+    case FIELD_MAX_LENGTH = "m";
+    case FIELD_TYPE = "ty";
+    case FIELD_D = "d";
+
+    case ROW_INFO = "r_i";
+    case ROW = "r";
 
 }
