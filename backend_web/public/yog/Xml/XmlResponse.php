b8e84e280e7a12780e925279895af16602dcfa64
Eduardo A.F. <eacevedof@yahoo.es>
2025-03-23 22:03:55 +0100

    xml reader ok

--- backend_web/public/yog/Xml/XmlResponse.php
+++ backend_web/public/yog/Xml/XmlResponse.php
@@ -3,6 +3,7 @@
 namespace Yog\Xml;
 
 use Yog\Bootstrap\ConstantEnum;
+use Yog\Enums\XmlTagEnum;
 
 final class XmlResponse
 {
@@ -12,10 +13,36 @@ final class XmlResponse
         return new self();
     }
 
-    public function add()
+    public function getTagValue(XmlTagEnum $tag, string $value): string
     {
-
+        return $this->getTaggedValue($tag->value, $value);
     }
 
+    private function getTaggedValue(string $tag, string $value): string
+    {
+        $value = htmlentities($value, ENT_QUOTES, "UTF-8");
+        return "<$tag>$value</$tag>";
+    }
 
+    private function addOutput(string $tagged): self
+    {
+        $this->output[] = $tagged;
+        return $this;
+    }
+
+    public function reset(): self
+    {
+        $this->output = [];
+        return $this;
+    }
+
+    public function getOutput(): string
+    {
+        return implode("", $this->output);
+    }
+
+    public function echoOutput(): void
+    {
+        echo $this->getOutput();
+    }
 }
