3972779fb32036f5a4568c603b72e33aec097454
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-14 19:49:09 +0200

    headers

--- backend_web/app/middleware.php
+++ backend_web/app/middleware.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 use Slim\App;
 use App\Slim\Application\Middleware\SessionMiddleware;
+use App\Slim\Infrastructure\Middlewares\HeadersMiddleware;
 
 return function (App $app) {
     $app->add(SessionMiddleware::class);
+    $app->add(HeadersMiddleware::class);
 };
---
+++ backend_web/src/Slim/Infrastructure/Middlewares/HeadersMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Slim\Infrastructure\Middlewares;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\MiddlewareInterface as Middleware;
+use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+
+final readonly class HeadersMiddleware implements Middleware
+{
+    public function process(Request $request, RequestHandler $handler): Response
+    {
+        $response = $handler->handle($request);
+        return $response->withHeader(
+            "Strict-Transport-Security",
+            "max-age=31536000; includeSubDomains; preload"
+        );
+    }
+}
