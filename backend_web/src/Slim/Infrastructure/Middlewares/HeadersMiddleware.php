73dfb24b657cbed45a5c8f2f5b263cc3f9d45f84
Eduardo A.F. <eacevedof@yahoo.es>
2025-05-14 20:14:50 +0200

    headers

--- backend_web/src/Slim/Infrastructure/Middlewares/HeadersMiddleware.php
+++ backend_web/src/Slim/Infrastructure/Middlewares/HeadersMiddleware.php
@@ -9,16 +9,30 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 
-final readonly class HeadersMiddleware implements Middleware
+final class HeadersMiddleware implements Middleware
 {
+    private array $headers = [
+        "Strict-Transport-Security" => "max-age=31536000; includeSubDomains; preload",
+        "X-Frame-Options" => "SAMEORIGIN",
+        "Set-Cookie" => "example_cookie=value; HttpOnly; SameSite=Strict",
+    ];
+
     public function process(Request $request, RequestHandler $handler): Response
     {
         $response = $handler->handle($request);
+        if ($request->getUri()->getScheme() === "https") {
+            $this->headers["Set-Cookie"] = "example_cookie=value; Secure; HttpOnly; SameSite=Strict";
+        }
 
-        return $response->withHeader(
-            "Strict-Transport-Security",
-            "max-age=31536000; includeSubDomains; preload"
-        );
-
+        return $this->getResponseWithHeaders($response);
     }
+
+    private function getResponseWithHeaders(Response $response): Response
+    {
+        foreach ($this->headers as $name => $value) {
+            $response = $response->withHeader($name, $value);
+        }
+        return $response;
+    }
+
 }
